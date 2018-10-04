4caa03886707f82b034acb1b5251806db05ef754
EC2 Default User <wlyman@coa.edu>
2018-10-04 15:55:17 +0000

    troll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,21 @@ class ApplicationController < ActionController::Base
     render html: "This is MY LAND"
   end
   def goodbye
-    render html: "Wadely Man Land"
+    render html: "
+    Wadely Man Land
+    a
+    d
+    e
+    l 
+    y 
+    
+    M 
+    a 
+    n 
+    
+    L
+    a
+    n
+    d"
   end
 end
